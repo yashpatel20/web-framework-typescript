e93dcba45b8d837e9d14123b362242e317f42c68
Yashpatel20 <yash.work10@gmail.com>
2020-05-09 14:14:58 +0530

    set up

---
+++ src/models/User.ts
@@ -0,0 +1,21 @@
+interface UserProps {
+  name?: string;
+  age?: number;
+}
+
+type Callback = () => {};
+
+export class User {
+  events: { [key: string]: Callback[] } = {};
+
+  constructor(private data: UserProps) {}
+  get(propName: string): string | number {
+    return this.data[propName];
+  }
+
+  set(update: UserProps): void {
+    Object.assign(this.data, update);
+  }
+
+  on(eventName: string, callback: Callback) {}
+}
