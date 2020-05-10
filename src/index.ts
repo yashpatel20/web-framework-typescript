c19250b72ab5858839b90c2e760ae2e5b4d5fc8d
Yashpatel20 <yash.work10@gmail.com>
2020-05-10 19:57:21 +0530

    added collection class

--- src/index.ts
+++ src/index.ts
@@ -1,18 +1,10 @@
-//Goal is to develop a web framework
-//when a user is updated, dispatch a change event and notify other components to rerender
-//typescript principles so that the code scales
+import { Collection } from "./models/Collection";
+import { User, UserProps } from "./models/User";
 
-import { User } from "./models/User";
+const collection = User.buildUserCollection();
 
-//creating a new user
-const user = User.buildUser({ id: 1 });
-
-//adding an event listener to user
-user.on("change", () => {
-  console.log(user);
+collection.on("change", () => {
+  console.log(collection);
 });
 
-//new user is saved into the db
-user.fetch();
-
-user.isAdminUser();
+collection.fetch();
